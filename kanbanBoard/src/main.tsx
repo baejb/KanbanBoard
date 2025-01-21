ed2ba7b7a242df47b035b830a28c79a405b1b2aa
baejb <bjb6478@naver.com>
2025-01-21 21:17:02 +0900

    style : global style 설정

--- kanbanBoard/src/main.tsx
+++ kanbanBoard/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './store/index.ts';
-import './index.css';
+import './styles/global.css';
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
