0513b041962e6c9ae860b1bef1abb0501aac27d8
baejb <bjb6478@naver.com>
2025-01-21 21:17:42 +0900

    feat : 공통 컴포넌트 Title 구현

---
+++ kanbanBoard/src/components/common/Title.tsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+interface TitleType {
+  fontSize?: string;
+  color?: string;
+  fontWeight?: string;
+  children: React.ReactNode;
+}
+
+const TitleText = styled.div<TitleType>`
+  font-size: ${({ fontSize }) => fontSize || '1.5rem'};
+  color: ${({ color }) => color || '#000'};
+  font-weight: ${({ fontWeight }) => fontWeight || 'normal'};
+`;
+const Title = ({ fontSize, color, fontWeight, children }: TitleType) => {
+  return (
+    <TitleText fontSize={fontSize} color={color} fontWeight={fontWeight}>
+      {children}
+    </TitleText>
+  );
+};
+
+export default Title;
