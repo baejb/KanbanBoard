12b38da7414dc186a59e35e23f928efbfa2ca96a
baejb <bjb6478@naver.com>
2025-01-22 19:33:33 +0900

    style: color 객체 생성

---
+++ kanbanBoard/src/styles/color.ts
@@ -0,0 +1,16 @@
+const colors = {
+  BACKLOG: '#ddf5ff',
+  TODO: '#dbfbe1',
+  INPROGRESS: '#fff8c5',
+  DONE: '#fbefff',
+  BACKLOG_BORDER: '#458de4',
+  TODO_BORDER: '#298c45',
+  INPROGRESS_BORDER: '#aa8034',
+  DONE_BORDER: '#8251df',
+  BOARD_COUNT: '#5f6974',
+  BOARD_COUNT_BACKGROUND: '#e7ebee',
+  BOARD_BACKGROUND: '#f7f8fa',
+  BOARD_BACKGROUND_BORDER: '#eff2f4',
+};
+
+export default colors;
